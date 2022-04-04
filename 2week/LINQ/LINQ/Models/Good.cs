454a87caeab74f6b1053ffed54ac2b73b270e6c1
Aidar <aidarkadyraliev7@gmail.com>
2022-04-04 10:21:19 +0300

    Create a DataSeeed in linq

--- 2week/LINQ/LINQ/Models/Good.cs
+++ 2week/LINQ/LINQ/Models/Good.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace LINQ.Models
@@ -12,11 +13,24 @@ namespace LINQ.Models
         public string Category { get; private set; }
         public string CountryOfOrigin { get; private set; }
 
+
         public Good(string articleNumber, string category, string countryOfOrigin)
         {
-            ArticleNumber = articleNumber;
+            ArticleNumber = CheckTheArticle(articleNumber);
             Category = category;
             CountryOfOrigin = countryOfOrigin;
+        }
+        private string CheckTheArticle(string aricleNumber)
+        {
+            string pattern = @"AA00-0000";
+            // Create a Regex  
+            Regex rg = new Regex(pattern);
+            if (string.IsNullOrEmpty(aricleNumber))
+                throw new ArgumentNullException(nameof(aricleNumber));
+            if(rg.IsMatch(aricleNumber))
+                return aricleNumber;
+            throw new ArgumentException();
+
         }
     }
 }
---
+++ 2week/LINQ/LINQ/Services/DataSeed.cs
@@ -0,0 +1,68 @@
+﻿using LINQ.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ.Services
+{
+    public class DataSeed
+    {
+        public IEnumerable<Consumer> Consumers()
+        {
+            List<Consumer> consumers = new List<Consumer>()
+            {
+                new Consumer(1, DateTime.Now.AddYears(-20), "KR"),
+                new Consumer(2, DateTime.Now.AddYears(-21), "Russia"),
+                new Consumer(3, DateTime.Now.AddYears(-22), "KZ"),
+                new Consumer(4, DateTime.Now.AddYears(-23), "USA"),
+                new Consumer(5, DateTime.Now.AddYears(-20), "KR")
+            };
+            return consumers;
+        }
+        public IEnumerable<ConsumerDiscount> ConsumerDiscounts()
+        {
+            List<ConsumerDiscount> consumerDiscounts = new List<ConsumerDiscount>()
+            {
+                new ConsumerDiscount(1, "Amazon", 12),
+                new ConsumerDiscount(2, "AliBaba", 20),
+                new ConsumerDiscount(2, "AliExpress", 15),
+                new ConsumerDiscount(3, "Amazon", 3),
+                new ConsumerDiscount(3, "AliExpress", 15),
+                new ConsumerDiscount(4, "AliExpress", 5),
+                new ConsumerDiscount(5, "Amazon", 12),
+            };
+            return consumerDiscounts;
+        }
+        public IEnumerable<Good> Goods()
+        {
+            List<Good> goods = new List<Good>()
+            {
+                new Good("AA12-1212", "Laptop", "China"),
+                new Good("QA14-1115", "GPU", "Korea"),
+                new Good("OP32-1231", "CPU", "USA"),
+                new Good("LP12-5412", "Laptop", "China"),
+                new Good("PC42-3412", "PC", "USA"),
+                new Good("QA14-1111", "GPU", "Korea"),
+                new Good("MN72-6782", "Monitor", "Tailand"),
+            };
+            return goods;
+        }
+        public IEnumerable<PriceOfGood> PriceOfGoods()
+        {
+            List<PriceOfGood> priceOfGoods = new List<PriceOfGood>()
+            {
+                new PriceOfGood("AA12-1212", "Amazon",1200),
+                new PriceOfGood("QA14-1115", "AliBaba",800),
+                new PriceOfGood("OP32-1231", "AliExpress",900),
+                new PriceOfGood("LP12-5412", "AliBaba",1000),
+                new PriceOfGood("PC42-3412", "Amazon",750),
+                new PriceOfGood("QA14-1111", "AliExpress",800),
+                new PriceOfGood("MN72-6782", "AleExpress",600),
+            };
+            return priceOfGoods;
+        }
+
+    }
+}
