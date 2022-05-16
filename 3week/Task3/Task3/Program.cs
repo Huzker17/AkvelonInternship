e594ad375645ba56fdacc04e22c0104354232562
Huzker17 <88390592+Huzker17@users.noreply.github.com>
2022-05-16 08:36:19 +0300

    Update Program.cs

--- 3week/Task3/Task3/Program.cs
+++ 3week/Task3/Task3/Program.cs
@@ -1,6 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Task3;
 
+Mutex mutexObj = new Mutex()
+mutexObj.WaitOne();
 Console.WriteLine("Hello, World!");
 ThreadMaanger ThreadManager = new ThreadMaanger();
 ThreadManager.Enter();
+mutexObj.ReleaseMutex();
